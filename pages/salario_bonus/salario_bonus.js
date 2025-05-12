fb1439fecc4b49ddf6796a23b27fcf56957ec23b
Arthur Vendramini <vendraminiarthur2008@gmail.com>
2025-05-12 00:12:04 +0000

    correção exercicio 3

--- pages/salario_bonus/salario_bonus.js
+++ pages/salario_bonus/salario_bonus.js
@@ -1,6 +1,10 @@
-function salario(salarioFixo, totalVendas){
+function bonus(totalVendas){
     let porcentagem = 0.15
     let bonus = totalVendas * porcentagem
+    return bonus
+}
+
+function salario(salarioFixo, totalVendas){
     let salarioTotal = salarioFixo + bonus
     return salarioTotal
 }
@@ -10,5 +14,5 @@ let funcionario2 = salario(700, 0)
 let funcionario3 = salario(1700, 1230.5)
 
 console.log(`TOTAL 1 = R$ ${funcionario1.toFixed(2)}`)
-console.log(`TOTAL 1 = R$ ${funcionario2.toFixed(2)}`)
-console.log(`TOTAL 1 = R$ ${funcionario3.toFixed(2)}`)
+console.log(`TOTAL 2 = R$ ${funcionario2.toFixed(2)}`)
+console.log(`TOTAL 3 = R$ ${funcionario3.toFixed(2)}`)
